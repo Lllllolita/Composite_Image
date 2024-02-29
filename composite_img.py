1def6b82aa7c775121cd918f33c6304555742524
Lllllolita <45601002+Lllllolita@users.noreply.github.com>
2024-02-29 16:46:10 +0800

    Update composite_img.py
    update

--- composite_img.py
+++ composite_img.py
@@ -4,8 +4,9 @@ from sklearn.cluster import AffinityPropagation
 from scipy.spatial import KDTree
 import math
 import random
-# np.random.seed(0)
-# random.seed(0)
+from functools import reduce
+np.random.seed(8)
+random.seed(8)
 def is_not_overlapped(tar_min_X, tar_max_X, tar_min_Y, tar_max_Y, d_min_X, d_max_X, d_min_Y, d_max_Y):
     if tar_max_X < d_min_X or tar_min_X > d_max_X or tar_max_Y < d_min_Y or tar_min_Y > d_max_Y:
         return True
@@ -20,10 +21,14 @@ def get_crop_fg(mask, img):
     y2 = np.max(points[0])
     return mask.crop((x1, y1, x2, y2)), img.crop((x1, y1, x2, y2))
 
-bg_img_path = "Images\\000849.jpg"
+bg_img_path = "Images\\003997.jpg"
+out_cluster_path = "Output_cluster\\003997_yh.jpg"
+# out_cluster_path = "Output_cluster\\003997_xy.jpg"
+# out_cluster_path = "Output_cluster\\003997_xyh.jpg"
 fg_img_path = "fg\\fg.jpg"
 mask_img_path = "fg\\mask.jpg"
-box_path = "Annotations\\000849.jpg.txt"
+out_result_path = "Output_result\\003997_3.jpg"
+box_path = "Annotations\\003997.jpg.txt"
 
 bg_img = Image.open(bg_img_path)
 width, height = bg_img.size
@@ -43,6 +48,8 @@ for i, point in enumerate(boxes):
 
 box_arr = np.array(boxes)
 X = box_arr[:, 4:6]
+# X = box_arr[:, 6:]
+# X = box_arr[:, 5:]
 model = AffinityPropagation(damping=0.5)
 model.fit(X)
 
@@ -51,7 +58,7 @@ clusters = np.unique(yhat)
 h_arr = []
 cluster_bg_img = bg_img.copy()
 draw = ImageDraw.Draw(cluster_bg_img)
-base_color = 0
+base_color = 60
 color_num = len(clusters)
 color_bar = int(255 / color_num)
 count = 0
@@ -65,7 +72,9 @@ for cluster in clusters:
     draw.rectangle([(draw_x1, draw_y1), (draw_x2, draw_y2)], fill=None, outline=outline_color, width=3)
     mean_h = np.mean(iter_data[:, 5])
     h_arr.append(mean_h)
-cluster_bg_img.show()
+# cluster_bg_img.show()
+# cluster_bg_img.save(out_cluster_path)
+
 h_arr = np.array(h_arr)
 sorted_h_arr = np.argsort(-h_arr)
 print(sorted_h_arr)
@@ -87,14 +96,14 @@ while(cycle):
         res_y = int(tar_point[7] + random.uniform(0, res_h * 0.1))
     else:
         other_clusters_idx = sorted_h_arr[: np.argwhere(tar_cluster == sorted_h_arr)[0][0]]
-        other_cluster_data = []
+        other_cluster_data = dict()
         for i in other_clusters_idx:
             data_idx = np.where(yhat == i)
-            other_cluster_data.append(np.take(box_arr, data_idx, axis=0)[0])
+            other_cluster_data[i] = np.take(box_arr, data_idx, axis=0)[0]
         overlap_clusters_idx = []
         tar_min_X, tar_max_X = np.min(tar_points[:, 1]), np.max(tar_points[:, 2])
         tar_min_Y, tar_max_Y = np.min(tar_points[:, 3]), np.max(tar_points[:, 4])
-        for i, d in enumerate(other_cluster_data):
+        for i, d in other_cluster_data.items():
             d_min_X, d_max_X = np.min(d[:, 1]), np.max(d[:, 2])
             d_min_Y, d_max_Y = np.min(d[:, 3]), np.max(d[:, 4])
             if is_not_overlapped(tar_min_X, tar_max_X, tar_min_Y, tar_max_Y, d_min_X, d_max_X, d_min_Y, d_max_Y):
@@ -102,6 +111,7 @@ while(cycle):
             else:
                 overlap_clusters_idx.append(i)
         if len(overlap_clusters_idx) == 0:
+            print("stage2")
             cycle = False
             tar_point_idx = np.argmax(tar_points[:, 5])
             tar_point = tar_points[tar_point_idx]
@@ -115,7 +125,7 @@ while(cycle):
                 tree = KDTree(other_cluster_data[i][:, 6:])
                 pairs = []
                 k_num = math.ceil(other_cluster_data[i].shape[0] / 2)
-                k_sequence = [i + 1for i in range(k_num)]
+                k_sequence = [ii + 1 for ii in range(k_num)]
                 cur_no_lapped_points = []
                 for j, point in enumerate(tar_points):
                     flag = True
@@ -161,16 +171,17 @@ while(cycle):
                             else:
                                 min_x_range[str(j)][0] = max(min_x_range[str(j)][0], np.min(tar_points[:, 1]))
                                 min_x_range[str(j)][1] = min(min_x_range[str(j)][1], np.max(tar_points[:, 2]))
-                if len(no_lapped_points) == 0:
-                    no_lapped_points = cur_no_lapped_points
-                else:
-                    no_lapped_points = list(set(no_lapped_points) & set(cur_no_lapped_points))
-            if len(no_lapped_points) > 0:
+                no_lapped_points.append(cur_no_lapped_points)
+            no_lapped_points_arr = [np.array(l) for l in no_lapped_points]
+            no_lapped_points_final = reduce(np.intersect1d, no_lapped_points_arr)
+            if len(no_lapped_points_final) > 0:
+                print("stage3")
                 cycle = False
-                tar_point_idx = random.choice(no_lapped_points)
+                tar_point_idx = random.choice(no_lapped_points_final)
                 tar_point = tar_points[tar_point_idx]
                 res_h = int(tar_point[5] * random.uniform(1, 1.1))
-                res_y = min(int(tar_point[7] + random.uniform(0, res_h * 0.1)), np.max(tar_points[:, 7]))
+                # res_y = min(int(tar_point[7] + random.uniform(0, res_h * 0.1)), np.max(tar_points[:, 7]))
+                res_y = np.max(tar_points[:, 7])
                 res_x = random.randint(min_x_range[str(tar_point_idx)][0] + int(res_h * 0.2), min_x_range[str(tar_point_idx)][1] - int(res_h * 0.2))
             else:
                 clusters = np.delete(clusters, np.where(clusters == tar_cluster))
@@ -186,3 +197,4 @@ fg_mask = fg_mask.resize(res_size)
 point = (max(0, int(res_x - res_size[0] // 2)), max(0, int(res_y - res_size[1] // 2)))
 bg_img.paste(fg_img, point, mask=fg_mask)
 bg_img.show()
+bg_img.save(out_result_path)
